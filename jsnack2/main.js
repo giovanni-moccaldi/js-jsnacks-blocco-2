efb279202b821b2fd40b045ae6d4e14d6390ada9
giovanni-moccaldi <giovannimoccaldi@gmail.com>
2023-01-28 11:52:27 +0100

    ...

---
+++ jsnack2/main.js
@@ -0,0 +1,12 @@
+const numeri = ["2", "23", "74", "15", "52", "64"];
+console.log(numeri);
+for (let i = 0; i < numeri.length; i++) {
+    const numerini = numeri[i];
+    if (numerini % 2 !== 0) {
+        numerini = element.classList;
+        list.add("verde");
+
+    }
+
+
+}
