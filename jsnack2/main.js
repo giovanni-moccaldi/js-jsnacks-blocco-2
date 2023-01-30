01c40291e8534cb0ffe084c49c56f2a56fc48c45
giovanni-moccaldi <giovannimoccaldi@gmail.com>
2023-01-30 18:52:30 +0100

    correct

--- jsnack2/main.js
+++ jsnack2/main.js
@@ -1,12 +1,14 @@
+const greenelement = document.getElementById("verde");
+const redelement = document.getElementById("rosso");
 const numeri = ["2", "23", "74", "15", "52", "64"];
 console.log(numeri);
 for (let i = 0; i < numeri.length; i++) {
     const numerini = numeri[i];
-    if (numerini % 2 !== 0) {
-        numerini = element.classList;
-        list.add("verde");
-
+    if (numerini % 2 == 0) {
+        greenelement.append(numerini);
     }
 
-
+    else {
+        redelement.append(numerini);
+    }
 }
